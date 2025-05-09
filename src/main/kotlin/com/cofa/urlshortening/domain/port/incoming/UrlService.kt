7db2f30155eafbe9fce093ee3fe1330d160b6d06
Mohammad Nazemzadeh <mohammad.nazem@gmail.com>
2025-05-09 15:20:06 +0200

    feat(domain): add Url model and port interfaces

--- src/main/kotlin/com/cofa/urlshortening/domain/port/incoming/UrlService.kt
+++ src/main/kotlin/com/cofa/urlshortening/domain/port/incoming/UrlService.kt
@@ -1,4 +1,6 @@
 package com.cofa.urlshortening.domain.port.incoming
 
 interface UrlService {
+    fun shortenUrl(originalUrl: String): String
+    fun getOriginalUrl(shortCode: String): String
 }
--- src/main/kotlin/com/cofa/urlshortening/domain/port/outgoing/UrlRepository.kt
+++ src/main/kotlin/com/cofa/urlshortening/domain/port/outgoing/UrlRepository.kt
@@ -1,4 +1,9 @@
 package com.cofa.urlshortening.domain.port.outgoing
 
+import com.cofa.urlshortening.domain.model.ShortUrl
+
 interface UrlRepository {
+    fun save(url: ShortUrl): ShortUrl
+    fun findByShortCode(shortCode: String): ShortUrl?
+    fun findByOriginalUrl(originalUrl: String): ShortUrl?
 }
